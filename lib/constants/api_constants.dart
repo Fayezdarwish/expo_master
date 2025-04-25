479d3bb88d370c17a1dfa469ee5a761d06923cb5
Fayez Darwish <fayezdarwish@gmail.com>
2025-04-25 19:30:30 +0300

    finish working api login and registar

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,3 +1,2 @@
-const String baseUrl = "http://localhost:3000";
-
+const String baseUrl = 'http://192.168.57.153:3000';
 
--- lib/utils/token_storage.dart
+++ lib/utils/token_storage.dart
@@ -1,16 +1,21 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class TokenStorage {
+  // حفظ التوكن بعد إزالة كلمة Bearer إن وجدت
   static Future<void> saveToken(String token) async {
     final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('token', token);
+    // إزالة كلمة Bearer والمسافة إن وجدت
+    final cleanedToken = token.replaceFirst('Bearer ', '');
+    await prefs.setString('token', cleanedToken);
   }
 
+  // استرجاع التوكن المخزن
   static Future<String?> getToken() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString('token');
   }
 
+  // حذف التوكن عند تسجيل الخروج
   static Future<void> clearToken() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('token');
