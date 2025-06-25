74523a45037497a5d965120b5c41966800908f17
Fayez Darwish <fayezdarwish@gmail.com>
2025-06-25 16:09:40 +0300

    updit main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:expo_master/vendor_sections/ExhibitorDepartmentsScreen.dart';
-import 'package:expo_master/vendor_sections/ExhibitorRequestFormScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:expo_master/config/theme.dart';
 
@@ -10,30 +8,23 @@ import 'package:expo_master/screens_login_and_registar/forgot_password_screen.da
 import 'package:expo_master/screens_login_and_registar/reset_password_screen.dart';
 
 // شاشات المسؤول (Admin)
+import 'Features/create_department_screen.dart';
+import 'Features/manage_departments_screen.dart';
+import 'Features/sinup_screen_for_manger_section.dart';
+import 'Features/welcome_screen.dart';
+
+// شاشات العارض (Exhibitor)
 import 'Exhibitor/ExhibitorRequestFormPage.dart';
 import 'Exhibitor/RentPaymentPage.dart';
 import 'Exhibitor/RequestStatusPage.dart';
-import 'Features/create_department_screen.dart';
-import 'Features/sinup_screen_for_manger_section.dart';
-import 'Features/welcome_screen.dart';
-import 'Features/manage_departments_screen.dart';
 
 // شاشات مدير القسم (Section Manager)
 import 'manager/screen/RequestDetailsPage.dart';
-import 'manager/screen/add_wing_page.dart';
-import 'manager/screen/edit_wing_page.dart';
-import 'manager/screen/requests_list_page.dart';
 import 'manager/screen/section_manager_home.dart';
 import 'manager/screen/wing_list_page.dart';
-
-// شاشات العارض (Exhibitor)
-import 'package:expo_master/vendor_sections/vendor_booth_tasks_screen.dart';
-import 'package:expo_master/vendor_sections/vendor_create_booth_screen.dart';
-import 'package:expo_master/vendor_sections/vendor_final_payment_screen.dart';
-import 'package:expo_master/vendor_sections/vendor_initial_payment_screen.dart';
-import 'package:expo_master/vendor_sections/vendor_request_form.dart';
-import 'package:expo_master/vendor_sections/vendor_requests_status_screen.dart';
-import 'package:expo_master/vendor_sections/vendor_sections_screen.dart';
+import 'manager/screen/requests_list_page.dart';
+import 'manager/screen/add_wing_page.dart';
+import 'manager/screen/edit_wing_page.dart';
 
 void main() {
   runApp(const ExpoMasterApp());
@@ -49,41 +40,32 @@ class ExpoMasterApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.themeData,
       initialRoute: '/login',
-
       routes: {
-        // شاشات تسجيل الدخول والمصادقة
+        // 🔐 المصادقة
         '/login': (context) => const LoginScreen(),
         '/register': (context) => const SignUpScreen(),
         '/forgot-password': (context) => const ForgotPasswordScreen(),
         '/reset-password': (context) => const ResetPasswordScreen(),
 
-        // شاشات المسؤول (Admin)
+        // 👨‍💼 المسؤول
         '/admin/welcome': (context) => const WelcomeScreen(),
         '/ManageDepartmentsScreen': (context) => const ManageDepartmentsScreen(),
         '/SignUpManagerScreen': (context) => const SignUpManagerScreen(),
 
-        // شاشات مدير القسم (Section Manager)
+        // 🧑‍💼 مدير القسم
         '/section-manager/home': (context) => const SectionManagerHome(),
         '/section-manager/wings': (context) => const WingListPage(),
-       // '/section-manager/wings/add': (context) => const AddWingPage(),
-        // '/section-manager/wings/edit': (context) => const EditWingPage(),
+      //  '/section-manager/wings/add': (context) => const AddWingPage(),
+      //  '/section-manager/wings/edit': (context) => const EditWingPage(),
         '/section-manager/requests': (context) => const RequestsListPage(),
+      //  '/section-manager/request-details': (context) => const RequestDetailsPage(),
 
-        // شاشات العارض (Exhibitor)
-        '/exhibitor/home': (context) =>  VendorSectionsScreen(),
-        '/exhibitorRequestForm': (context) => ExhibitorRequestFormPage(),
-        '/requestStatus': (context) => RequestStatusPage(),
-        '/rentPayment': (context) => RentPaymentPage(),
-          '/exhibitorDepartments': (context) => const ExhibitorDepartmentsScreen(),
-          '/exhibitorRequestForm': (context) => const ExhibitorRequestFormScreen(),
-        '/VendorInitialPaymentScreen': (context) => VendorInitialPaymentScreen(),
-        '/VendorRequestsStatusScreen': (context) => VendorRequestsStatusScreen(),
-        '/VendorSectionsScreen': (context) => VendorSectionsScreen(),
-       // '/VendorBoothTasksScreen': (context) => VendorBoothTasksScreen(),
-       // '/VendorCreateBoothScreen': (context) => VendorCreateBoothScreen(),
-       // '/VendorFinalPaymentScreen': (context) => VendorFinalPaymentScreen(),
+        // 🏢 العارض
+        '/exhibitor/request-form': (context) => ExhibitorRequestFormPage(),
+        '/exhibitor/request-status': (context) => RequestStatusPage(),
+        '/exhibitor/rent-payment': (context) => RentPaymentPage(),
 
-        // صفحة الزائر - مؤقتة Placeholder
+        // 🌐 الزائر (صفحة مؤقتة)
         '/visitor/home': (context) => const Placeholder(),
       },
     );
