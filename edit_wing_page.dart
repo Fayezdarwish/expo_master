4a679c40d71b699caf03367d6783612d6cdc6354
esraayousef12 <esraaalhagyousef@gmail.com>
2025-06-26 15:50:32 +0300

    new
    add new screen

---
+++ edit_wing_page.dart
@@ -0,0 +1,50 @@
+  import 'package:flutter/material.dart';
+  import '../../services/api_service.dart';
+
+  class EditWingPage extends StatefulWidget {
+    final Map<String, dynamic> wing;
+    const EditWingPage({super.key, required this.wing});
+
+    @override
+    State<EditWingPage> createState() => _EditWingPageState();
+  }
+
+  class _EditWingPageState extends State<EditWingPage> {
+    late TextEditingController nameController;
+
+    @override
+    void initState() {
+      super.initState();
+      nameController = TextEditingController(text: widget.wing['name']);
+    }
+
+    Future<void> updateWing() async {
+      await ApiService.putWithToken('/wings/${widget.wing['id']}', {
+        'name': nameController.text,
+      }, 'YOUR_TOKEN');
+      Navigator.pop(context);
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('تعديل الجناح')),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(labelText: 'اسم الجناح'),
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: updateWing,
+                child: const Text('حفظ التعديلات'),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+  }
